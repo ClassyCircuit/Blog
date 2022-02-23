3425c64a026810214e593867831f4f888fb3b181
Jānis Simsons <janis.simsons@if.lv>
2022-02-23 16:53:32 +0200

    Debugging program.cs

--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NReco.Logging.File;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Blog.Web
 {
@@ -11,6 +16,19 @@ namespace Blog.Web
 		public static void Main(string[] args)
 		{
 			var host = CreateHostBuilder(args).Build();
+
+			var file = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			var factory = new LoggerFactory();
+			factory.AddProvider(new FileLoggerProvider(file));
+			var logger = factory.CreateLogger("T");
+
+			logger.LogInformation("Testing");
+
+			var dbUser = Environment.GetEnvironmentVariable("DbUser");
+			var dbPass = Environment.GetEnvironmentVariable("DbPass");
+
+			logger.LogInformation(dbUser);
+			logger.LogInformation(dbPass);
 
 			using (var scope = host.Services.CreateScope())
 			{
